2921c1272b9cea479c88e5733782781eb3821615
Valentina Svichnik <svichnikvalentina@gmail.com>
2020-06-25 15:44:33 +0300

    pfnel

--- resources/views/panel.blade.php
+++ resources/views/panel.blade.php
@@ -9,6 +9,7 @@
     <div class="row justify-content-start">
       <div class="col-12">
         <h1 class="mt-5 mb-4">Панель администратора</h1>
+        <h5>text</h5>
       </div>
     </div>
   </div>
